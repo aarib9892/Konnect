ae2d6ae4ddf306575f176122790de74a382c4f37
aarib <aarib9892@gmail.com>
2024-11-15 17:14:13 +0530

    setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/Landing";
+import Room from "./components/Room";
+
+function App() {
+  return (
+    <BrowserRouter >
+      <Routes>
+        <Route path="/room" element={<Room />} />
+
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
