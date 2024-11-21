b06ffbe30dd17f678eda3022cab0739ffe0af976
aarib <aarib9892@gmail.com>
2024-11-21 17:50:11 +0530

    Websocket offer exchange complete

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,9 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useEffect , useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { io } from "socket.io-client";
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = 'http://localhost:3000';
+
+
+
 const Room = () => {
-  let [searchParams, setSearchParams] = useSearchParams();
-  let name = searchParams.get("name");
-  useEffect(() => {}, [name]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [lobby, setLobby] = useState(false)
+  const name = searchParams.get("name");
+
+  useEffect(() => {
+   const socket = io(URL);
+
+    socket.on('connect',()=>{
+      alert('connected')
+    })
+
+    socket.on('send-offer',({roomId}) => {
+      alert('please send offer')
+      socket.emit('offer',{
+        roomId
+      })
+    })
+
+    socket.on('offer',({roomId,offer})=>{
+      alert('please send answerr')
+      socket.emit('answer',{
+        roomId,offer
+      })
+    })
+
+    socket.on('answer',({roomId,offer})=>{
+      alert('Connection Established')
+
+    })
+
+    socket.on('lobby',() => {
+      setLobby(true)
+    })
+  }, [name]);
+
+
+  if(lobby){
+    return <div>Waiting for you to connect with someone ....</div>
+  }
 
   return (
     <>
