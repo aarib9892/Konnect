cd3f596d7b9ebe0095b7f9482e3e2d9166a626e1
aarib <aarib9892@gmail.com>
2024-11-26 21:41:28 +0530

    acquiring local media tracks

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,10 +1,37 @@
-import React  , {useState }from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
   const [name, setName] = useState("");
+  const localVideoRef = useRef(null);
+  const [localAudioTrack, setLocalAudioTrack] = useState();
+  const [localVideoTrack, setLocalVideoTrack] = useState();
+  const [remoteAudioTrack, setRemoteAudioTrack] = useState();
+  const [remoteVideoTrack, setremoteVideoTrack] = useState();
+
+  const getPermission = async () => {
+    let stream = await window.navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    let videoTrack = stream.getVideoTracks()[0];
+    let audioTrack = stream.getAudioTracks()[0];
+    setLocalAudioTrack(audioTrack)
+    setLocalVideoTrack(videoTrack)
+    localVideoRef.current.srcObject = new MediaStream([videoTrack]);
+    localVideoRef.current.play=true
+
+    
+  };
+
+  useEffect(() => {
+    if (localVideoRef?.current) {
+      getPermission();
+    }
+  }, [localVideoRef]);
   return (
     <div>
+      <video autoPlay ref={localVideoRef}></video>
       <input onChange={(e) => setName(e.target.value)} type="text" />
       <Link to={`/room/?name=${name}`}>Join</Link>
     </div>
